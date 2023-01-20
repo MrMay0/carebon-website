5c34c02d5e90fe974fc60a872d5eba7459c2a47f
Jean-Loup <jlarette-22@telecom-paris.fr>
2023-01-20 10:53:19 +0100

    ajout fonctionnalités

--- serveur/server_test.py
+++ serveur/server_test.py
@@ -2,42 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-class Question:
-    def __init__(self, text, choices, values):
-        self.text = text
-        self.choices = choices
-        self.values = values
-
-def discrete_to_func(choices, values):
-    def func(choice):
-        for i in range(len(choices)):
-            if choices[i] == choice:
-                return values[i]
-        return "non valide"
-    return lambda x: func(x)
-
-questionnaire = {'1': Question("Combien de mètre carrés fait la maison ?", ("20 m²", "40m²", "100m²"),
-                discrete_to_func(("20", "40", "100"), (200, 400, 1000))),
-                '2': Question("Distance parcourue ?", None, lambda x: 10*x)}
-
 @app.route("/")
 def hello_world():
-    return "<p> Hello, World! </p>"
+    return "Pact groupe 6.1\nConsEcolo"
 
 @app.route("/somme/<a>/<b>")
 def somme(a, b):
-    return f"<p> {a} + {b} = {int(a)+int(b)} </p>"
+    return f"{a} + {b} = {int(a)+int(b)}"
 
 @app.route("/deplacement")
 def deplacement():
-    return "<p> Déplacement </p>"
+    return "Déplacement"
 
 @app.route("/reponse/<question>/<reponse>")
 def reponse(question, reponse):
-    print(question, reponse)
-    print(type(question), type(reponse))
-    ans = questionnaire[question].values(reponse)
-    return f"<p> {ans} </p>"
+    return "reponse"
 
 @app.route("/getQuestion")
 def getQuestion():
@@ -58,6 +37,26 @@ def achat():
 @app.route("/delete")
 def delete():
     return "delete"
+
+@app.route("/getConso")
+def delete():
+    return "Votre consommation"
+
+@app.route("/getRank")
+def delete():
+    return "Votre classement"
+
+@app.route("/getGlobal")
+def delete():
+    return "Consommation Globale"
+
+@app.route("/setArticle")
+def delete():
+    return "Donner la consommation d'un article"
+
+@app.route("/createAccount")
+def delete():
+    return "Créer un compte"
 
 if __name__ == "__main__":
     app.run()
