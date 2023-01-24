2e4267d960168043da03ba66c87887c307e807a2
Ulysse Ristorcelli <ristorcelli.ulysse@gmail.com>
2023-01-24 09:49:11 +0100

    ajout page d'accueil

--- serveur/server_test.py
+++ serveur/server_test.py
@@ -1,15 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import table
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "Pact groupe 6.1\nConsEcolo"
 
+
 @app.route("/somme/<a>/<b>")
 def somme(a, b):
     return f"{a} + {b} = {int(a)+int(b)}"
+
 
 @app.route("/sommeQuerry")
 def sommeQ():
@@ -17,45 +20,66 @@ def sommeQ():
     b = int(request.args.get('b'))
     return f"{a} + {b} = {a+b}"
 
+
 @app.route("/deplacement")
 def deplacement():
     return "Déplacement"
+
 
 @app.route("/reponse/<question>/<reponse>")
 def reponse(question, reponse):
     return "reponse"
 
+
 @app.route("/getQuestion")
 def getQuestion():
     return "question"
+
 
 @app.route("/getConso")
 def getConso():
     return str(table.getConso(request.args.get('id')))
 
+
 @app.route("/classement")
 def classement():
     return table.getRank()
+
 
 @app.route("/achat")
 def achat():
     return "achat"
 
+
 @app.route("/delete")
 def delete():
     return "delete"
+
 
 @app.route("/getGlobal")
 def getGlobal():
     return str(table.getMean())
 
+
 @app.route("/setArticle")
 def setArticle():
     return "Donner la consommation d'un article"
 
+
 @app.route("/createAccount")
 def createAccount():
     return "Créer un compte"
+
+
+@app.route("/getBadge")
+def getBadge():
+    return "Vos Badges"
+
+
+@app.route("/homePage")
+def goHome():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
